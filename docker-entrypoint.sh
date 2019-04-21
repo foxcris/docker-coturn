4f2446471a24c84864dbc7844dffe2f9a5968812
root <root@vmd38354.contaboserver.net>
2019-04-22 01:09:46 +0200

    - enable coturn server - fix bash scripting errors

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,93 +8,93 @@ echo "Starting cron server"
 #Check Parameters and create configurationfile (only on first start)
 if [ ! -e /etc/turnserver.conf ]
 then
-    if [ "$TURN_SERVER_NAME" -eq "" ]
+    if [ "$TURN_SERVER_NAME" = "" ]
     then
       TURN_SERVER_NAME=coturn
     fi
     echo "server-name ${TURN_SERVER_NAME}" > /etc/turnserver.conf
     
-    if [ "$TURN_SECRET" -eq "" ]
+    if [ "$TURN_SECRET" = "" ]
     then
       TURN_SECRET=`date +%s | sha256sum | base64 | head -c 64`
     fi
     echo "static-auth-secret ${TURN_SECRET}" >> /etc/turnserver.conf
     
-    if [ "$TURN_REALM" -eq "" ]
+    if [ "$TURN_REALM" = "" ]
     then
       TURN_REALM=default.realm
     fi
     echo "realm ${TURN_REALM}" >> /etc/turnserver.conf
     
-    if [ "$TURN_PORT" -eq "" ]
+    if [ "$TURN_PORT" = "" ]
     then
       TURN_PORT=8478
     fi
     echo "listening-port ${TURN_PORT}" >> /etc/turnserver.conf
     
-    if [ "$TURN_PORT_START" -eq "" ]
+    if [ "$TURN_PORT_START" = "" ]
     then
       TURN_PORT_START=49152
     fi
     echo "min-port ${TURN_PORT_START}" >> /etc/turnserver.conf
     
-    if [ "$TURN_PORT_END" -eq "" ]
+    if [ "$TURN_PORT_END" = "" ]
     then
       TURN_PORT_END=65535
     fi
     echo "max-port ${TURN_PORT_END}" >> /etc/turnserver.conf
     
-    if [ "$TURN_USE_AUTH_SECRET" -eq "" ] || [ "$TURN_USE_AUTH_SECRET" -eq "true" ]
+    if [ "$TURN_USE_AUTH_SECRET" = "" ] || [ "$TURN_USE_AUTH_SECRET" = "true" ]
     then
       echo "use-auth-secret" >> /etc/turnserver.conf
     fi
     
-    if [ "$TURN_NO_TCP_RELAY" -eq "" ] || [ "$TURN_NO_TCP_RELAY" -eq "true" ]
+    if [ "$TURN_NO_TCP_RELAY" = "" ] || [ "$TURN_NO_TCP_RELAY" = "true" ]
     then
       echo "no-tcp-relay" >> /etc/turnserver.conf
     fi
     
-    if [ "$TURN_LT_CRED_MECH" -eq "" ] || [ "$TURN_LT_CRED_MECH" -eq "true" ]
+    if [ "$TURN_LT_CRED_MECH" = "" ] || [ "$TURN_LT_CRED_MECH" = "true" ]
     then
       echo "lt-cred-mech" >> /etc/turnserver.conf
     fi
-    if [ "$TURN_USER_QUOTA" -eq "" ]
+    if [ "$TURN_USER_QUOTA" = "" ]
     then
       $TURN_USER_QUOTA = 12
     fi
     echo "user-quota ${TURN_USER_QUOTA}" >> /etc/turnserver.conf
     
-    if [ "$TURN_TOTAL_QUOTA" -eq "" ]
+    if [ "$TURN_TOTAL_QUOTA" = "" ]
     then
       $TURN_TOTAL_QUOTA = 1200
     fi
     echo "total-quota ${TURN_TOTAL_QUOTA}" >> /etc/turnserver.conf
     
-    if [ "$TURN_NO_STUN" -eq "" ] || [ "$TURN_NO_STUN" -eq "true" ]
+    if [ "$TURN_NO_STUN" = "" ] || [ "$TURN_NO_STUN" = "true" ]
     then
       echo "no-stun" >> /etc/turnserver.conf
     fi
-    if [ "$TURN_NO_MUTICAST_PEERS" -eq "" ] || [ "$TURN_NO_MUTICAST_PEERS" -eq "true" ]
+    if [ "$TURN_NO_MUTICAST_PEERS" = "" ] || [ "$TURN_NO_MUTICAST_PEERS" = "true" ]
     then
       echo "no-multicast-peers" >> /etc/turnserver.conf
     fi
-    if [ "$TURN_NO_TLSV1" -eq "" ] || [ "$TURN_NO_TLSV1" -eq "true" ]
+    if [ "$TURN_NO_TLSV1" = "" ] || [ "$TURN_NO_TLSV1" = "true" ]
     then
       echo "no-tlsv1" >> /etc/turnserver.conf
     fi
-    if [ "$TURN_NO_TLSV1_1" -eq "" ] || [ "$TURN_NO_TLSV1_1" -eq "true" ]
+    if [ "$TURN_NO_TLSV1_1" = "" ] || [ "$TURN_NO_TLSV1_1" = "true" ]
     then
       echo "no-tlsv1_1" >> /etc/turnserver.conf
     fi
-    if [ ! "$TURN_CERT_PATH" -eq "" ]
+    if [ ! "$TURN_CERT_PATH" = "" ]
     then
       echo "cert ${TURN_CERT_PATH}" >> /etc/turnserver.conf
     fi
-    if [ ! "$TURN_PKEY_PATH" -eq "" ]
+    if [ ! "$TURN_PKEY_PATH" = "" ]
     then
       echo "pkey ${TURN_PKEY_PATH}" >> /etc/turnserver.conf
     fi
-    if [ ! $TURN_DENIED_PEER_IP -eq "" ]
+    if [ ! $TURN_DENIED_PEER_IP = "" ]
     then
       peers=$(echo $TURN_DENIED_PEER_IP | tr "," "\n")
       for peer in $peers
@@ -112,4 +112,5 @@ echo "Starting TURN/STUN server"
 service coturn start
 
 #turnserver -v -L 0.0.0.0 --server-name "${TURN_SERVER_NAME}" --static-auth-secret="${TURN_SECRET}" --realm=${TURN_REALM}  -p ${TURN_PORT} --min-port ${TURN_PORT_START} --max-port ${TURN_PORT_END} --user-quota ${TURN_USER_QUOTA} --total-quota ${$TURN_TOTAL_QUOTA} ${TURN_USE_AUTH_SECRET} ${TURN_NO_TCP_RELAY} ${TURN_LT_CRED_MECH} ${TURN_NO_STUN} ${TURN_NO_MUTICAST_PEERS} ${TURN_CERT} ${TURN_PKEY} ${TURN_EXTRA}
+touch /var/log/turnserver
 tail -f /var/log/turnserver
